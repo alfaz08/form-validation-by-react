ee0222479b03ccdaa9648cf6143e1f9128720157
Alfaz Hossain <alfaz.cse@gmail.com>
2023-09-21 19:16:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+import StatefulForm from './components/StatefulForm/StatefulForm'
+import RefForm from './components/RefForm/RefForm'
+function App() {
+
+
+  return (
+    <>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
+
+    </>
+  )
+}
+
+export default App
