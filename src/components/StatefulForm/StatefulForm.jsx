d08184b46d165879bebd949c564b7efb38321c82
Alfaz Hossain <alfaz.cse@gmail.com>
2023-09-21 19:18:42 +0600

    custom hook and from validation with react

--- src/components/StatefulForm/StatefulForm.jsx
+++ src/components/StatefulForm/StatefulForm.jsx
@@ -1,57 +1,55 @@
 import { useState } from "react";
 
 
-const SimpleForm = () => {
-  const [name,setName] =useState(null)
-   const [email,setEmail] = useState(null)
-   const [password,setPassword] =useState(null)
-  const [error,setError] =useState('')
+const StatefulForm = () => {
+    const [name, setName] = useState('Rojoni Klanto');
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState('')
 
-  const handleSubmit = e =>{
-    e.preventDefault();
-    if(password.length <6){
-      setError('password must be 6 character')
-    }
-    else{
-      setError('')
-    }
-    console.log(email,password,name);
-
-  }
-  const handleNameChange = e=>{
-    setName(e.target.value)
-  }
-  const handleEmailChange = e =>{
- setEmail(e.target.value)
-  }
-  const handlePasswordChange = e=>{
-    setPassword(e.target.value)
-  }
-  
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input 
-        onChange={handleNameChange}
-        type="text" name="name"/>
-        <br />
-        <input 
-         onChange={handleEmailChange}
-        type="email" name="email"/>
-        <br />
-        <input 
-        onChange={handlePasswordChange}
-        type="password" name="password" required />
-        <br />
-        <input type="submit" value="submit" />
-
-        {
-          error && <p>{error}</p>
+    const handleSubmit = e =>{
+        e.preventDefault();
+        if(password.length < 6){
+            setError('Password must be 6 characters or longer')
         }
-      </form>
-    </div>
-  );
+        else{
+            setError('');
+            console.log(name, email, password);
+        }
+    }
+
+    const handleNameChange = e =>{
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = e =>{
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={handleNameChange} type="text" name="name" />
+                <br />
+                <input 
+                    onChange={handleEmailChange}
+                type="email" name="email" id="" />
+                <br />
+                <input 
+                    onChange={handlePasswordChange}
+                type="password" name="password" required />
+                <br />
+                <input type="submit" value="Submit" />
+                {
+                    error && <p>{error}</p>
+                }
+            </form>
+        </div>
+    );
 };
 
-export default SimpleForm;
+export default StatefulForm;
